931db8cc883f61594c8d150a555530d44e098bd9
hiren patel <hirenpatel0526@yahoo.com>
2022-09-14 14:56:13 -0700

    added js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordChoice = {
+  passwordLength : 0,
+  passwordLowerCase : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  passwordUpperCase : ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+  passwordNumber  :9 ['0','1','2','3','4','5','6','7','8','9']
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +18,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
