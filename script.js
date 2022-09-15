d6d2bb770c7a0d30221659c3c15594226f2542c1
hiren patel <hirenpatel0526@yahoo.com>
2022-09-14 17:42:57 -0700

    asking questions in js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function askQuestions(){
   numbers = confirm("Do you want to use numbers?");
   symbols = confirm ("Do you want to use symbols");
 }
-console.log(askQuestions)
+
+
 var passwordChoice = {
   passwordLength : 0,
   passwordLowerCase : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
@@ -14,6 +15,8 @@ var passwordChoice = {
   passwordNumbers  : ['0','1','2','3','4','5','6','7','8','9'],
   passwordSymbols : ['`','~','!','@','#','$','%','^','&','*','(',')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', '"', ',', '<', '.', '>', '/', '?']
 }
+
+
 
 
 // Write password to the #password input
@@ -25,5 +28,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword(){
+
+  var passwordLength = 0;
+  var upperCase;
+  var lowerCase;
+  var numbers;
+  var symbols;
+  passwordLength = 0;
+  passwordChoice.passwordlength = 0;
+
+  if (passwordLength < 8 || passwordLength > 128){
+    passwordLength =prompt("Your password must be more than 8 and less than 128 numbers!");
+    passwordSymbols =prompt("Do you want any characters in your password")
+    passwordLowercase = prompt("do you want any lowercase in your password")
+    password
+
+  
+  }
+  
+
+}
+console.log(generatePassword);
 
 
