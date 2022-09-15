c59fa02952e25972c2bc62919cfe1b3e669272a7
hiren patel <hirenpatel0526@yahoo.com>
2022-09-15 06:06:54 -0700

    final

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// Assignment Code
-  var generateBtn = document.querySelector("#generate");
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// // Add event listener to generate button
+//   generateBtn.addEventListener("click", writePassword); 
+
+// // Assignment Code
+//   var generateBtn = document.querySelector("#generate");
 
   var passwordLowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var passwordUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -19,37 +24,36 @@
     if (passwordLength <= 8 || passwordLength >= 128){
          alert("Your password must between 8 and 128 characters.");
          var passwordLength = (prompt("How many characters would you like to have in your password?"))
-         alert("")
     }
 
-    var symbols =prompt("Do you want any characters in your password?");
-    var numbers = prompt("Do you want any numbers in your password")
+    var symbols =prompt("Do you want any symbols in your password?");
+    var numbers = prompt("Do you want any numbers in your password");
     var lowerCase = prompt("Do you want any lowercase in your password?");
     var upperCase = prompt("do you want any uppercase in your password?");
 
     if (symbols === false && numbers === false && lowerCase === false && upperCase === false){
       prompt("You must choose atleast one out of all");
       var symbols =prompt("Do you want any characters in your password?");
-      var numbers = prompt("Do you want any numbers in your password")
+      var numbers = prompt("Do you want any numbers in your password");
       var lowerCase = prompt("Do you want any lowercase in your password?");
       var upperCase = prompt("do you want any uppercase in your password?");
     }
     var passwordCharacters = []
 
     if (symbols){
-      passwordCharacters = passwordCharacters.concat(symbols)
+      passwordCharacters = passwordCharacters.concat(passwordSymbols)
     }
     if (numbers){
-      passwordCharacters = passwordCharacters.concat(numbers)
+      passwordCharacters = passwordCharacters.concat(passwordNumbers)
     }
     if (lowerCase){
-      passwordCharacters = passwordCharacters.concat(lowerCase)
+      passwordCharacters = passwordCharacters.concat(passwordLowerCase)
     }
     if(upperCase){
-      passwordCharacters = passwordCharacters.concat(upperCase)
+      passwordCharacters = passwordCharacters.concat(passwordUpperCase)
     }
     var randomPassword = ""
-    for (var i =0; i < passwordLength; i==){
+    for (var i =0; i < passwordLength; i++){
       randomPassword =randomPassword + passwordCharacters[Math.floor(Math.random) * passwordCharacters.length];
     }
       return randomPassword;
@@ -65,5 +69,4 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
