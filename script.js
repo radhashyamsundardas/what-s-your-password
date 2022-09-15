d6e667f850c1b560045f39a14fab0babcbdf9025
hiren patel <hirenpatel0526@yahoo.com>
2022-09-15 05:24:23 -0700

    added questions to js

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-function askQuestions(){
-  lowerCase = confirm("Do you want to use lowercase letters?");
-  upperCase = confirm("Do you want to use uppercase letters?");
-  numbers = confirm("Do you want to use numbers?");
-  symbols = confirm ("Do you want to use symbols");
-}
+  var generateBtn = document.querySelector("#generate");
+
+  var passwordLowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var passwordUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var passwordNumbers  = ['0','1','2','3','4','5','6','7','8','9'];
+  var passwordSymbols = ['`','~','!','@','#','$','%','^','&','*','(',')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', '"', ',', '<', '.', '>', '/', '?']
 
 
-var passwordChoice = {
-  passwordLength : 0,
-  passwordLowerCase : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-  passwordUpperCase : ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-  passwordNumbers  : ['0','1','2','3','4','5','6','7','8','9'],
-  passwordCharacters : ['`','~','!','@','#','$','%','^','&','*','(',')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', '"', ',', '<', '.', '>', '/', '?']
-}
+  var passwordLength = "";
+  var symbols;
+  var numbers;
+  var upperCase;
+  var lowerCase;
 
+  function generatePassword(){
+    var passwordLength = (prompt("How many characters would you like to have in your password?"));
 
+    if (passwordLength <= 8 || passwordLength >= 128){
+         alert("Your password must between 8 and 128 characters.");
+         var passwordLength = (prompt("How many characters would you like to have in your password?"))
+         alert("")
+    }
 
+    var symbols =prompt("Do you want any characters in your password?");
+    var numbers = prompt("Do you want any numbers in your password")
+    var lowercase = prompt("Do you want any lowercase in your password?");
+    var uppercase = prompt("do you want any uppercase in your password?");
+
+    if (symbols === false && numbers === false && lowercase === false && uppercase === false){
+      prompt("You must choose atleast one out of all");
+      var symbols =prompt("Do you want any characters in your password?");
+      var numbers = prompt("Do you want any numbers in your password")
+      var lowercase = prompt("Do you want any lowercase in your password?");
+      var uppercase = prompt("do you want any uppercase in your password?");
+    }
+    
+    
+    }
 
 // Write password to the #password input
 function writePassword() {
@@ -28,27 +47,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-function generatePassword(){
-
-  var passwordLength = 0;
-  var upperCase;
-  var lowerCase;
-  var numbers;
-  var symbols;
-  passwordLength = 0;
-  passwordChoice.passwordlength = 0;
-
-  if (passwordLength < 8 || passwordLength > 128){
-    passwordLength =prompt("Your password must between 8 and 128 numbers!");
-    passwordcharacters =prompt("Do you want any characters in your password?");
-    passwordLowercase = prompt("Do you want any lowercase in your password?");
-    passwordUppercase = prompt("do you want any uppercase in your password?");
-    passwordNumbers = prompt("Do you want any numbers in your password")
-}
-}
-console.log(generatePassword);
-
-
